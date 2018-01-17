cad30ea8c7b7b03b8fb093facb8a0a05692c0103
Jay Bednar <ironviralmedia@gmail.com>
2018-01-17 20:53:24 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,8 +2,8 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init(e) {
   let i = 0;
-  const b = document.querySelector('keydown')
-  keyDown.addEventListener('keydown', function(e) {
+  const body1 = document.querySelector('body')
+  body1.addEventListener('keydown', function(e) {
     const key = parseInt(e.detail || e.which);
     if (key === code[i]) {
       i++;
