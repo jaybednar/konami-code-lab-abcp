147917a18a650592f8098a26694cc8e6d3fae9bb
Jay Bednar <ironviralmedia@gmail.com>
2018-01-17 20:35:53 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,6 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
+function init(e) {
   let index = 0;
   e.addEventHandler('keydown', function(e){
     const key = parseInt(e.detail || e.which);
