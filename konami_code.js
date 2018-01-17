5fbdffca460a3074c1471ea2b1963b0a2a90dbca
Jay Bednar <ironviralmedia@gmail.com>
2018-01-17 20:46:25 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,6 +3,6 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init(e) {
   const keyDown = document.querySelector('keydown')
   keyDown.addEventListener('keydown', function(e) {
-  console.log(e.which)
+    if ()
   });
 }
