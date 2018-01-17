8b1ee7c085941fc0986225a775be4e8e1463a64f
Jay Bednar <ironviralmedia@gmail.com>
2018-01-17 20:35:22 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -5,7 +5,7 @@ function init() {
   e.addEventHandler('keydown', function(e){
     const key = parseInt(e.detail || e.which);
     if (key === code[index]) {
-      
+      index++;
       if (index === code.length) {
         alert('Hurray!');
         index = 0;
