7afb3cac0a974eb15e054ecfd209c10c9f6faea1
Jay Bednar <ironviralmedia@gmail.com>
2018-01-17 20:32:33 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,10 +2,13 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init(e) {
   let index = 0;
+  e.addEventHandler('keydown', function onKeyDownHandler(e){
+    
+  })
   function onKeyDownHandler(e) {
     const key = parseInt(e.detail || e.which);
     if (key === code[index]) {
-      e.addEventHandler('keydown', function())
+      
       if (index === code.length) {
         alert('Hurray!');
         index = 0;
